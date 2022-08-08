af93df9e5e5e5f8b6898b4158cda28cdc7bd34d2
Mike L <mike.levy.rva@gmail.com>
2022-08-08 11:56:03 -0400

    express server created

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+// Dependencies
+
+const express = require("express");
+
+// Express configuration
+
+//Tells node that we are creating an 'express' server
+const app = express();
+
+// Sets an initial port.
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended : true}));
+// app.use(express.json);
+// app.use(express.static("public"));
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+})
