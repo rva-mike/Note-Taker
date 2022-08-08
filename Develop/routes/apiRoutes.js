9383c7116e452c9ecd6c15fe46c4647836d53021
Mike L <mike.levy.rva@gmail.com>
2022-08-08 12:11:09 -0400

    api post request for new note

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,12 +2,35 @@
 const notesData = require("../db/db.json");
 // const fs = require('fs');
 
-// Routing
+// routing
 module.exports = function (app, fs) {
 
-    // API GET Requests
-    app.get("/api/notes", function(request, response){
+    // API GET Request
+    app.get("/api/notes", (request, response) => {
 
         response.send(notesData);
+    });
+
+    // API POST Request
+    app.post("/api/notes", (request, response) => {
+
+        // take new note from request.  
+        const newNote = request.body;
+
+        // Pushed new note in db.json
+        notesData.push(newNote);
+
+        // notes data to db.json
+        fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
+
+            if(err) throw(err);
+
+            console.log("New note successfully added to db.json.");
+
+        });
+
+        console.log("Notes : "+JSON.stringify(notesData));
+
+        response.json(newNote);
     });
 };
