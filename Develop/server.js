3c0896c020ed2845eb8105ed3122e9b3ef043bca
Mike L <mike.levy.rva@gmail.com>
2022-08-08 12:05:57 -0400

    created apiRoutes.js and created routes to route files

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,27 @@
 // Dependencies
 
 const express = require("express");
+const fs = require("fs");
 
 // Express configuration
 
 //Tells node that we are creating an 'express' server
 const app = express();
 
-// Sets an initial port.
-const PORT = process.env.PORT || 3000;
+// sets up port for any port or 3001
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended : true}));
-// app.use(express.json);
-// app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// ROUTES
+require('./routes/apiRoutes')(app, fs);
+require('./routes/htmlRoutes')(app);
+
+
+
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
